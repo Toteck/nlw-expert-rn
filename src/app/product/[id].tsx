0b40f992ed49ff9b87286b4e4199ec3b4ed856ce
mateus <mateusweslley14@gmail.com>
2024-02-10 21:49:15 -0300

    Ajustes na visualização dos produtos

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,4 +1,4 @@
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, ScrollView } from "react-native";
 import { useLocalSearchParams, useNavigation } from "expo-router";
 import { PRODUCTS } from "@/utils/data/products";
 import { formatCurrency } from "@/utils/functions/format-currency";
@@ -28,41 +28,44 @@ export default function Product() {
   }
 
   return (
-    <View className="flex-1 border-solid border-2 border-red-600">
-      <Image
-        source={product.cover}
-        className="w-full h-52"
-        resizeMode="cover"
-      />
+    <View className="flex-1 ">
+      <ScrollView>
+        <Image
+          source={product.cover}
+          className="w-full h-52"
+          resizeMode="cover"
+        />
 
-      <Text className="text-white text-xl font-heading">{product.title}</Text>
-
-      <View className="p-5 mt-8 flex-1 border-solid border-2 border-sky-300">
-        <Text className="text-lime-400 text-2xl font-heading my-2">
-          {formatCurrency(product.price)}
-        </Text>
-        <Text className="text-slate-400 font-body text-base leading-6 mb-6">
-          {product.description}
-        </Text>
-        {product.ingredients.map((ingredient) => (
-          <Text
-            key={ingredient}
-            className="text-slate-400 font-body text-base leading-6"
-          >
-            {"\u2022"} {ingredient}
+        <View className="p-5 mt-8 flex-1 ">
+          <Text className="text-white text-xl font-heading">
+            {product.title}
           </Text>
-        ))}
-      </View>
+          <Text className="text-lime-400 text-2xl font-heading my-2">
+            {formatCurrency(product.price)}
+          </Text>
+          <Text className="text-slate-400 font-body text-base leading-6 mb-6">
+            {product.description}
+          </Text>
+          {product.ingredients.map((ingredient) => (
+            <Text
+              key={ingredient}
+              className="text-slate-400 font-body text-base leading-6"
+            >
+              {"\u2022"} {ingredient}
+            </Text>
+          ))}
+        </View>
 
-      <View className="p-5 pb-8 gap-5 ">
-        <Button onPress={handleAddToCart}>
-          <Button.Icon>
-            <Feather name="plus-circle" size={20} />
-          </Button.Icon>
-          <Button.Text>Adicionar ao pedido</Button.Text>
-        </Button>
-        <LinkButton title="Voltar ao cardápio" href="/" />
-      </View>
+        <View className="p-5 pb-8 gap-5 ">
+          <Button onPress={handleAddToCart}>
+            <Button.Icon>
+              <Feather name="plus-circle" size={20} />
+            </Button.Icon>
+            <Button.Text>Adicionar ao pedido</Button.Text>
+          </Button>
+          <LinkButton title="Voltar ao cardápio" href="/" />
+        </View>
+      </ScrollView>
     </View>
   );
 }
